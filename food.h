d456c4012bd78540dc756f801f96ed2c6be3de62
01prakash-aditya <167614956+01prakash-aditya@users.noreply.github.com>
2025-01-19 17:01:04 +0530

    Update food.h

--- food.h
+++ food.h
@@ -1,8 +1,14 @@
 #pragma once
+
 class Food {
 public:
-	double energy;
-	double productivity;
-	double cost;
-	enum foodstuff food;
+    double energy;
+    double productivity;
+    double cost;
+
+    enum class Foodstuff {
+        Donut,
+        Coffee,
+        Pizza,
+    } foodstuff;
 };
