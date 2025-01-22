4e0e6a0a4947ef6694cf7d01b6a282450bfa129c
Chirag Agrawal <raghavashishagrawal@gmail.com>
2025-01-22 09:55:10 +0530

    added senior worker class(base)

---
+++ seniorWorker.cpp
@@ -0,0 +1,27 @@
+#include "seniorWorker.h"
+
+//SpecialSoftware
+SeniorSoftware::SeniorSoftware(int index, short int x, short int y, short int gridnumber)
+    : Worker(index, x, y, gridnumber) {
+}
+SeniorSoftware::SeniorSoftware(int index, bool gender, char* name, short int x, short int y)
+    : Worker(index, gender, name, x, y) {
+}
+
+
+//SeniorHardware
+SeniorHardware::SeniorHardware(int index, short int x, short int y, short int gridnumber)
+    : Worker(index, x, y, gridnumber){
+}
+SeniorHardware::SeniorHardware(int index, bool gender, char* name, short int x, short int y)
+    : Worker(index, gender, name, x, y){
+}
+
+
+// SeniorNetwork 
+SeniorNetwork::SeniorNetwork(int index,short int x, short int y, short int gridnumber)
+    : Worker(index, x, y, gridnumber){
+}
+SeniorNetwork::SeniorNetwork(int index, bool gender, char* name, short int x, short int y)
+    : Worker(index, gender, name, x, y) {
+}
---
+++ seniorWorker.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "worker.h"
+
+
+class SeniorSoftware : public Worker {
+public:
+    SeniorSoftware(int index, short int x = 0, short int y = 0, short int gridnumber = 0);
+    SeniorSoftware(int index, bool gender, char* name, short int x = 0, short int y = 0);
+};
+
+
+class SeniorHardware :public Worker {
+public:
+    SeniorHardware(int index, short int x = 0, short int y = 0, short int gridnumber = 0);
+    SeniorHardware(int index, bool gender, char* name, short int x = 0, short int y = 0);
+};
+
+
+class SeniorNetwork :public Worker {
+public:
+    SeniorNetwork(int index, short int x = 0, short int y = 0, short int gridnumber = 0);
+    SeniorNetwork(int index, bool gender, char* name, short int x = 0, short int y = 0);
+};
+
