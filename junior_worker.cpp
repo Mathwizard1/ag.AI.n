f1dc0b764b242f90e966dcc3a6ddcb58045f0f8e
Chirag Agrawal <raghavashishagrawal@gmail.com>
2025-01-24 19:34:53 +0530

    added different functions and properties for junior workers. //Pushing now

--- junior_worker.cpp
+++ junior_worker.cpp
@@ -1,30 +1,58 @@
 #include "junior_worker.h"
 
-
-
-//SpecialSoftware
 JuniorSoftware::JuniorSoftware(int index, short int x, short int y, short int gridnumber)
-    : Worker(index, x, y, gridnumber){
-}
-JuniorSoftware::JuniorSoftware(int index,bool gender, char* name, short int x, short int y)
-    : Worker(index, gender, name, x, y){
+    : Worker(index, x, y, gridnumber), programmingSkills(50) {
 }
 
-//JuniorHardware
+JuniorSoftware::JuniorSoftware(int index, bool gender, char* name, short int x, short int y)
+    : Worker(index, gender, name, x, y), programmingSkills(50) {
+}
+
+void JuniorSoftware::makeObedient() {
+    obedient = true;  // add on
+}
+
+void JuniorSoftware::makeNonObedient() {
+    if (obedient == true) {
+        obedient = false;  //remove to get some money in need
+        money += 50;
+    }
+}
+
 JuniorHardware::JuniorHardware(int index, short int x, short int y, short int gridnumber)
-    : Worker(index, x, y, gridnumber){
+    : Worker(index, x, y, gridnumber), technicalSkills(50) {
 }
 
-JuniorHardware::JuniorHardware(int index,bool gender, char* name, short int x, short int y)
-    : Worker(index, gender, name, x, y){
+JuniorHardware::JuniorHardware(int index, bool gender, char* name, short int x, short int y)
+    : Worker(index, gender, name, x, y), technicalSkills(50) {
 }
 
-// JuniorNetwork 
+void JuniorHardware::makeObedient() {
+    obedient = true;  
+}
+
+void JuniorHardware::makeNonObedient() {
+    if (obedient == true) {
+        obedient = false; \
+        money += 50;
+    }
+}
+
 JuniorNetwork::JuniorNetwork(int index, short int x, short int y, short int gridnumber)
-    : Worker(index, x, y, gridnumber){
+    : Worker(index, x, y, gridnumber), networkingSkills(50) {
 }
 
-JuniorNetwork::JuniorNetwork(int index,bool gender, char* name, short int x, short int y)
-    : Worker(index, gender, name, x, y){
+JuniorNetwork::JuniorNetwork(int index, bool gender, char* name, short int x, short int y)
+    : Worker(index, gender, name, x, y), networkingSkills(50) {
 }
 
+void JuniorNetwork::makeObedient() {
+    obedient = true; 
+}
+
+void JuniorNetwork::makeNonObedient() {
+    if (obedient == true) {
+        obedient = false; 
+        money += 50;
+    }
+}
--- junior_worker.h
+++ junior_worker.h
@@ -1,20 +1,43 @@
 #pragma once
-#include "worker.h"
+#include <string>
+#include "worker.h"  
 
-class JuniorSoftware: public Worker {
+class JuniorSoftware : public Worker {
 public:
-    JuniorSoftware(int index,short int x =0, short int y = 0, short int gridnumber = 0);
+    JuniorSoftware(int index, short int x = 0, short int y = 0, short int gridnumber = 0);
     JuniorSoftware(int index, bool gender, char* name, short int x = 0, short int y = 0);
+
+    // Spec attr.
+    int programmingSkills;
+    bool obedient = false;
+
+    // extra addons to buy
+    void makeObedient();
+    void makeNonObedient();
 };
 
-class JuniorHardware :public Worker {
+class JuniorHardware : public Worker {
 public:
-    JuniorHardware(int index,short int x =0, short int y = 0, short int gridnumber = 0);
+    JuniorHardware(int index, short int x = 0, short int y = 0, short int gridnumber = 0);
     JuniorHardware(int index, bool gender, char* name, short int x = 0, short int y = 0);
+
+    // Specific attri.
+    int technicalSkills;
+    bool obedient = false;
+    // add ons
+    void makeObedient();
+    void makeNonObedient();
 };
 
 class JuniorNetwork : public Worker {
 public:
-    JuniorNetwork(int index,short int x =0, short int y = 0, short int gridnumber = 0);
+    JuniorNetwork(int index, short int x = 0, short int y = 0, short int gridnumber = 0);
     JuniorNetwork(int index, bool gender, char* name, short int x = 0, short int y = 0);
+
+    // spec attr.
+    int networkingSkills;
+    bool obedient = false;
+    // add ons
+    void makeObedient();
+    void makeNonObedient();
 };
