7c0f975487301fb280cd53847d81835a9857d00f
Mathwizard1 <anshurup.gupta@gmail.com>
2025-01-26 13:28:14 +0530

    .

--- Events.h
+++ Events.h
@@ -1,5 +1,16 @@
 #pragma once
+#include "gameConst.h"
+#include "Events.h"
+
+
 class Events
 {
+private:
+	int *timeFrame;
+	std::unordered_map<std::string, std::pair<bool, std::string>> pastRecords;
+
+public:
+	Events();
+
 };
 
