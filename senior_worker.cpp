a0e5287bf22e5619c4440311611c6f5682e13958
Chirag Agrawal <raghavashishagrawal@gmail.com>
2025-01-30 12:20:19 +0530

    senior worker class

--- senior_worker.cpp
+++ senior_worker.cpp
@@ -1,16 +1,50 @@
 #include "senior_worker.h"
+#include <iostream>
 
 
-
-//SpecialSoftware
 SeniorSoftware::SeniorSoftware(int index, short int x, short int y, short int gridnumber)
     : Worker(index, x, y, gridnumber) {
 }
+
 SeniorSoftware::SeniorSoftware(int index, bool gender, char* name, short int x, short int y)
     : Worker(index, gender, name, x, y) {
 }
 
-//SeniorHardware
+void SeniorSoftware::boostJuniorProductivity(std::vector<JuniorSoftware*>& juniors) {
+    for (auto& junior : juniors) {
+        junior->productivity += junior->productivity * 0.1;  //10% for base all numbers will change 
+    }
+}
+
+void SeniorSoftware::applyGamble(std::vector<Worker*>& workers) {
+    isgambler = true;
+    gamble();
+}
+
+void SeniorSoftware::applyEnergyBoost(Worker& target) {
+    target.energy += 20; 
+}
+
+void SeniorSoftware::applyLeadershipBoost(std::vector<JuniorSoftware*>& juniors) {
+    for (auto& junior : juniors) {
+        junior->productivity += 15;  
+        junior->mood += 10;  
+    }
+}
+
+void SeniorSoftware::applySelectiveStatBoost(std::vector<JuniorSoftware*>& juniors, const std::string& stat) {
+    if (stat == "productivity") {
+        for (auto& junior : juniors) {
+            junior->productivity += 10;
+        }
+    }
+}
+
+void SeniorSoftware::gamble() {
+    
+}
+
+// Base constructor for Senior Hardware Worker
 SeniorHardware::SeniorHardware(int index, short int x, short int y, short int gridnumber)
     : Worker(index, x, y, gridnumber) {
 }
@@ -19,11 +53,84 @@ SeniorHardware::SeniorHardware(int index, bool gender, char* name, short int x, 
     : Worker(index, gender, name, x, y) {
 }
 
-// SeniorNetwork 
+void SeniorHardware::boostHardwareSkills(std::vector<JuniorHardware*>& juniors) {
+    for (auto& junior : juniors) {
+        junior->productivity += junior->productivity * 0.1;  // Increase by 10%
+    }
+}
+
+void SeniorHardware::applyGamble(std::vector<Worker*>& workers) {
+    std::cout << "Applying Gambler Power: Boosting hardware skills for a random worker!" << std::endl;
+    gamble();
+}
+
+void SeniorHardware::applyEnergyBoost(Worker& target) {
+    target.energy += 20; // Boost energy by 20
+}
+
+void SeniorHardware::applyLeadershipBoost(std::vector<JuniorHardware*>& juniors) {
+    for (auto& junior : juniors) {
+        junior->productivity += 15;  // Boost productivity
+        junior->mood += 10;  // Boost mood
+    }
+}
+
+void SeniorHardware::applySelectiveStatBoost(std::vector<JuniorHardware*>& juniors, const std::string& stat) {
+    if (stat == "productivity") {
+        for (auto& junior : juniors) {
+            junior->productivity += 10;
+        }
+    }
+}
+
+void SeniorHardware::gamble() {
+    std::cout << "Gambler is applying a random boost!" << std::endl;
+    // Gambling logic to apply a random boost (example: boosting productivity)
+    // In real game scenario, this might be more dynamic or random
+}
+
+
+
+// Base constructor for Senior Network Worker
 SeniorNetwork::SeniorNetwork(int index, short int x, short int y, short int gridnumber)
     : Worker(index, x, y, gridnumber) {
 }
 
 SeniorNetwork::SeniorNetwork(int index, bool gender, char* name, short int x, short int y)
     : Worker(index, gender, name, x, y) {
+}
+
+void SeniorNetwork::boostNetworkSkills(std::vector<JuniorNetwork*>& juniors) {
+    for (auto& junior : juniors) {
+        junior->productivity += junior->productivity * 0.1;  // Increase by 10%
+    }
+}
+
+void SeniorNetwork::applyGamble(std::vector<Worker*>& workers) {
+    std::cout << "Applying Gambler Power: Boosting network skills for a random worker!" << std::endl;
+    gamble();
+}
+
+void SeniorNetwork::applyEnergyBoost(Worker& target) {
+    target.energy += 20; // Boost energy by 20
+}
+
+void SeniorNetwork::applyLeadershipBoost(std::vector<JuniorNetwork*>& juniors) {
+    for (auto& junior : juniors) {
+        junior->productivity += 15;  // Boost productivity
+        junior->mood += 10;  // Boost mood
+    }
+}
+
+void SeniorNetwork::applySelectiveStatBoost(std::vector<JuniorNetwork*>& juniors, const std::string& stat) {
+    if (stat == "productivity") {
+        for (auto& junior : juniors) {
+            junior->productivity += 10;
+        }
+    }
+}
+
+void SeniorNetwork::gamble() {
+   //time based 
+
 }
--- senior_worker.h
+++ senior_worker.h
@@ -1,20 +1,60 @@
 #pragma once
 #include "worker.h"
+#include <vector>
+#include "junior_worker.h"
+
 
 class SeniorSoftware : public Worker {
 public:
     SeniorSoftware(int index, short int x = 0, short int y = 0, short int gridnumber = 0);
     SeniorSoftware(int index, bool gender, char* name, short int x = 0, short int y = 0);
+
+    // power -up can be purchased
+    bool isgambler = false;
+    void boostJuniorProductivity(std::vector<JuniorSoftware*>& juniors);
+    void applyGamble(std::vector<Worker*>& workers);
+    void applyEnergyBoost(Worker& target);
+    void applyLeadershipBoost(std::vector<JuniorSoftware*>& juniors);
+    void applySelectiveStatBoost(std::vector<JuniorSoftware*>& juniors, const std::string& stat);
+    void gamble();
+    void callgamble();
 };
 
-class SeniorHardware :public Worker {
+class SeniorHardware : public Worker {
 public:
     SeniorHardware(int index, short int x = 0, short int y = 0, short int gridnumber = 0);
     SeniorHardware(int index, bool gender, char* name, short int x = 0, short int y = 0);
+
+    // power-up
+    void boostHardwareSkills(std::vector<JuniorHardware*>& juniors);
+    void applyGamble(std::vector<Worker*>& workers);
+    void applyEnergyBoost(Worker& target);
+    void applyLeadershipBoost(std::vector<JuniorHardware*>& juniors);
+    void applySelectiveStatBoost(std::vector<JuniorHardware*>& juniors, const std::string& stat);
+
+private:
+    void gamble();
 };
 
 class SeniorNetwork : public Worker {
 public:
     SeniorNetwork(int index, short int x = 0, short int y = 0, short int gridnumber = 0);
     SeniorNetwork(int index, bool gender, char* name, short int x = 0, short int y = 0);
+
+    //power_up
+    void boostNetworkSkills(std::vector<JuniorNetwork*>& juniors);
+    void applyGamble(std::vector<Worker*>& workers);
+    void applyEnergyBoost(Worker& target);
+    void applyLeadershipBoost(std::vector<JuniorNetwork*>& juniors);
+    void applySelectiveStatBoost(std::vector<JuniorNetwork*>& juniors, const std::string& stat);
+
+private:
+    void gamble();
 };
+
+
+
+
+
+
+
