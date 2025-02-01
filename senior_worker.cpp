c7bdb5f124e8dc2500023f72117c5e121a20bfae
Chirag Agrawal <raghavashishagrawal@gmail.com>
2025-02-01 11:35:24 +0530

    Senior worker pushed(gamble function remaining)

--- senior_worker.cpp
+++ senior_worker.cpp
@@ -10,41 +10,122 @@ SeniorSoftware::SeniorSoftware(int index, bool gender, char* name, short int x, 
     : Worker(index, gender, name, x, y) {
 }
 
-void SeniorSoftware::boostJuniorProductivity(std::vector<JuniorSoftware*>& juniors) {
-    for (auto& junior : juniors) {
-        junior->productivity += junior->productivity * 0.1;  //10% for base all numbers will change 
-    }
-}
-
-void SeniorSoftware::applyGamble(std::vector<Worker*>& workers) {
-    isgambler = true;
-    gamble();
-}
-
-void SeniorSoftware::applyEnergyBoost(Worker& target) {
-    target.energy += 20; 
-}
-
-void SeniorSoftware::applyLeadershipBoost(std::vector<JuniorSoftware*>& juniors) {
-    for (auto& junior : juniors) {
-        junior->productivity += 15;  
-        junior->mood += 10;  
-    }
-}
-
-void SeniorSoftware::applySelectiveStatBoost(std::vector<JuniorSoftware*>& juniors, const std::string& stat) {
+void SeniorSoftware::juniorStatsBoostByPercentage(std::vector<JuniorSoftware*>& juniors,int percentage,std::string &stat) {
+    money -= int((percentage/100) * 3/2);
     if (stat == "productivity") {
         for (auto& junior : juniors) {
-            junior->productivity += 10;
+            if (junior->obedient)
+                junior->productivity += junior->productivity * percentage/100;
+            junior->productivity = min(100, junior->productivity);
+        }
+    }
+    else if (stat == "energy") {
+        for (auto& junior : juniors) {
+            if (junior->obedient)
+                junior->energy += junior->energy *percentage/100;
+            junior->energy = min(100, junior->energy);
+        }
+    }
+    else if (stat == "mood") {
+        for (auto& junior : juniors) {
+            if (junior->obedient)
+                junior->mood += junior->mood * percentage/100;
+            junior->mood = min(100, junior->mood);
+        }
+    }
+    else if (stat == "health") {
+        for (auto& junior : juniors) {
+            if (junior->obedient)
+                junior->health +junior->health * percentage/100;
+            junior->health = min(100, junior->health);
         }
     }
 }
 
-void SeniorSoftware::gamble() {
+void SeniorSoftware::juniorstatsboostbyval(std::vector<JuniorSoftware*>& juniors, const std::string& stat, int val) {
+    money -= val * 2;
+    if (stat == "productivity") {
+        for (auto& junior : juniors) {
+            if (junior->obedient)
+                junior->productivity += val;
+            junior->productivity = min(100, junior->productivity);
+        }
+    }
+    else if (stat == "energy") {
+        for (auto& junior : juniors) {
+            if (junior->obedient)
+                junior->energy += val;
+            junior->energy = min(100, junior->energy);
+        }
+    }
+    else if (stat == "mood") {
+        for (auto& junior : juniors) {
+            if (junior->obedient)
+                junior->mood += val;
+            junior->mood = min(100, junior->mood);
+        }
+    }
+    else if (stat == "health") {
+        for (auto& junior : juniors) {
+            if (junior->obedient)
+                junior->health += val;
+            junior->health = min(100, junior->health);
+        }
+    }
+}
+//target --> curr senior worker
+void SeniorSoftware::applySelfStatBoost(Worker& target,const std::string & stat , int val) {
+    money -= val * 3;
+    if (stat == "productivity") {
+        target.productivity += val;
+        target.productivity = max(target.productivity, 100);
+    }
+    else if (stat == "energy") {
+        target.energy += val;
+        target.energy = max(target.energy, 100);
+    }
+    else if (stat == "mood") {
+        target.mood += val;
+        target.mood = max(target.mood, 100);
+    }
+    else if (stat == "health") {
+        target.health+= val;
+        target.health= max(target.health, 100);
+    }
     
 }
 
-// Base constructor for Senior Hardware Worker
+void SeniorSoftware::applyLeadershipBoost(std::vector<JuniorSoftware*>& juniors) {
+    money -= 50;
+    for (auto& junior : juniors) {
+        if(junior->obedient)
+        junior->productivity = max(100,junior->productivity+ int((junior->productivity)*0.2));  
+        junior->energy = max(100, junior->energy + int((junior->energy) * 0.2));
+        junior->mood = max(100, junior->mood + int((junior->mood) * 0.2));
+    }
+}
+//target --> curr senior worker
+void SeniorSoftware::medassistance(JuniorSoftware* junior, Worker& target) {
+    target.health -= 10;
+    junior->health = 100;
+}
+
+void SeniorSoftware::gamble() {
+    //need to implement//getting some errors
+}
+void SeniorSoftware::applyGamble(std::vector<Worker*>& workers) {
+    isgambler = true;
+    money -= 50;
+    gamble();
+}
+void SeniorSoftware::callgamble() {
+    //need to implement
+}
+
+
+
+
+
 SeniorHardware::SeniorHardware(int index, short int x, short int y, short int gridnumber)
     : Worker(index, x, y, gridnumber) {
 }
@@ -53,45 +134,124 @@ SeniorHardware::SeniorHardware(int index, bool gender, char* name, short int x, 
     : Worker(index, gender, name, x, y) {
 }
 
-void SeniorHardware::boostHardwareSkills(std::vector<JuniorHardware*>& juniors) {
-    for (auto& junior : juniors) {
-        junior->productivity += junior->productivity * 0.1;  // Increase by 10%
-    }
-}
-
-void SeniorHardware::applyGamble(std::vector<Worker*>& workers) {
-    std::cout << "Applying Gambler Power: Boosting hardware skills for a random worker!" << std::endl;
-    gamble();
-}
-
-void SeniorHardware::applyEnergyBoost(Worker& target) {
-    target.energy += 20; // Boost energy by 20
-}
-
-void SeniorHardware::applyLeadershipBoost(std::vector<JuniorHardware*>& juniors) {
-    for (auto& junior : juniors) {
-        junior->productivity += 15;  // Boost productivity
-        junior->mood += 10;  // Boost mood
-    }
-}
-
-void SeniorHardware::applySelectiveStatBoost(std::vector<JuniorHardware*>& juniors, const std::string& stat) {
+void SeniorHardware::juniorStatsBoostByPercentage(std::vector<JuniorHardware*>& juniors, int percentage, std::string& stat) {
+    money -= int((percentage / 100) * 3 / 2);
     if (stat == "productivity") {
         for (auto& junior : juniors) {
-            junior->productivity += 10;
+            if (junior->obedient)
+                junior->productivity += junior->productivity * percentage / 100;
+            junior->productivity = min(100, junior->productivity);
+        }
+    }
+    else if (stat == "energy") {
+        for (auto& junior : juniors) {
+            if (junior->obedient)
+                junior->energy += junior->energy * percentage / 100;
+            junior->energy = min(100, junior->energy);
+        }
+    }
+    else if (stat == "mood") {
+        for (auto& junior : juniors) {
+            if (junior->obedient)
+                junior->mood += junior->mood * percentage / 100;
+            junior->mood = min(100, junior->mood);
+        }
+    }
+    else if (stat == "health") {
+        for (auto& junior : juniors) {
+            if (junior->obedient)
+                junior->health + junior->health * percentage / 100;
+            junior->health = min(100, junior->health);
         }
     }
 }
 
+void SeniorHardware::juniorstatsboostbyval(std::vector<JuniorHardware*>& juniors, const std::string& stat, int val) {
+    money -= val * 2;
+    if (stat == "productivity") {
+        for (auto& junior : juniors) {
+            if (junior->obedient)
+                junior->productivity += val;
+            junior->productivity = min(100, junior->productivity);
+        }
+    }
+    else if (stat == "energy") {
+        for (auto& junior : juniors) {
+            if (junior->obedient)
+                junior->energy += val;
+            junior->energy = min(100, junior->energy);
+        }
+    }
+    else if (stat == "mood") {
+        for (auto& junior : juniors) {
+            if (junior->obedient)
+                junior->mood += val;
+            junior->mood = min(100, junior->mood);
+        }
+    }
+    else if (stat == "health") {
+        for (auto& junior : juniors) {
+            if (junior->obedient)
+                junior->health += val;
+            junior->health = min(100, junior->health);
+        }
+    }
+}
+//target --> curr senior worker
+void SeniorHardware::applySelfStatBoost(Worker& target, const std::string& stat, int val) {
+    money -= val * 3;
+    if (stat == "productivity") {
+        target.productivity += val;
+        target.productivity = max(target.productivity, 100);
+    }
+    else if (stat == "energy") {
+        target.energy += val;
+        target.energy = max(target.energy, 100);
+    }
+    else if (stat == "mood") {
+        target.mood += val;
+        target.mood = max(target.mood, 100);
+    }
+    else if (stat == "health") {
+        target.health += val;
+        target.health = max(target.health, 100);
+    }
+
+}
+
+void SeniorHardware::applyLeadershipBoost(std::vector<JuniorHardware*>& juniors) {
+    money -= 50;
+    for (auto& junior : juniors) {
+        if (junior->obedient)
+            junior->productivity = max(100, junior->productivity + int((junior->productivity) * 0.2));
+        junior->energy = max(100, junior->energy + int((junior->energy) * 0.2));
+        junior->mood = max(100, junior->mood + int((junior->mood) * 0.2));
+    }
+}
+//target --> curr senior worker
+void SeniorHardware::medassistance(JuniorHardware* junior, Worker& target) {
+    target.health -= 10;
+    junior->health = 100;
+}
+
 void SeniorHardware::gamble() {
-    std::cout << "Gambler is applying a random boost!" << std::endl;
-    // Gambling logic to apply a random boost (example: boosting productivity)
-    // In real game scenario, this might be more dynamic or random
+    //need to implement//getting some errors
+}
+void SeniorHardware::applyGamble(std::vector<Worker*>& workers) {
+    isgambler = true;
+    money -= 50;
+    gamble();
+}
+void SeniorHardware::callgamble() {
+    //need to implement
 }
 
 
 
-// Base constructor for Senior Network Worker
+
+
+
+
 SeniorNetwork::SeniorNetwork(int index, short int x, short int y, short int gridnumber)
     : Worker(index, x, y, gridnumber) {
 }
@@ -100,37 +260,114 @@ SeniorNetwork::SeniorNetwork(int index, bool gender, char* name, short int x, sh
     : Worker(index, gender, name, x, y) {
 }
 
-void SeniorNetwork::boostNetworkSkills(std::vector<JuniorNetwork*>& juniors) {
-    for (auto& junior : juniors) {
-        junior->productivity += junior->productivity * 0.1;  // Increase by 10%
-    }
-}
-
-void SeniorNetwork::applyGamble(std::vector<Worker*>& workers) {
-    std::cout << "Applying Gambler Power: Boosting network skills for a random worker!" << std::endl;
-    gamble();
-}
-
-void SeniorNetwork::applyEnergyBoost(Worker& target) {
-    target.energy += 20; // Boost energy by 20
-}
-
-void SeniorNetwork::applyLeadershipBoost(std::vector<JuniorNetwork*>& juniors) {
-    for (auto& junior : juniors) {
-        junior->productivity += 15;  // Boost productivity
-        junior->mood += 10;  // Boost mood
-    }
-}
-
-void SeniorNetwork::applySelectiveStatBoost(std::vector<JuniorNetwork*>& juniors, const std::string& stat) {
+void SeniorNetwork::juniorStatsBoostByPercentage(std::vector<JuniorNetwork*>& juniors, int percentage, std::string& stat) {
+    money -= int((percentage / 100) * 3 / 2);
     if (stat == "productivity") {
         for (auto& junior : juniors) {
-            junior->productivity += 10;
+            if (junior->obedient)
+                junior->productivity += junior->productivity * percentage / 100;
+            junior->productivity = min(100, junior->productivity);
+        }
+    }
+    else if (stat == "energy") {
+        for (auto& junior : juniors) {
+            if (junior->obedient)
+                junior->energy += junior->energy * percentage / 100;
+            junior->energy = min(100, junior->energy);
+        }
+    }
+    else if (stat == "mood") {
+        for (auto& junior : juniors) {
+            if (junior->obedient)
+                junior->mood += junior->mood * percentage / 100;
+            junior->mood = min(100, junior->mood);
+        }
+    }
+    else if (stat == "health") {
+        for (auto& junior : juniors) {
+            if (junior->obedient)
+                junior->health + junior->health * percentage / 100;
+            junior->health = min(100, junior->health);
         }
     }
 }
 
-void SeniorNetwork::gamble() {
-   //time based 
+void SeniorNetwork::juniorstatsboostbyval(std::vector<JuniorNetwork*>& juniors, const std::string& stat, int val) {
+    money -= val * 2;
+    if (stat == "productivity") {
+        for (auto& junior : juniors) {
+            if (junior->obedient)
+                junior->productivity += val;
+            junior->productivity = min(100, junior->productivity);
+        }
+    }
+    else if (stat == "energy") {
+        for (auto& junior : juniors) {
+            if (junior->obedient)
+                junior->energy += val;
+            junior->energy = min(100, junior->energy);
+        }
+    }
+    else if (stat == "mood") {
+        for (auto& junior : juniors) {
+            if (junior->obedient)
+                junior->mood += val;
+            junior->mood = min(100, junior->mood);
+        }
+    }
+    else if (stat == "health") {
+        for (auto& junior : juniors) {
+            if (junior->obedient)
+                junior->health += val;
+            junior->health = min(100, junior->health);
+        }
+    }
+}
+//target --> curr senior worker
+void SeniorNetwork::applySelfStatBoost(Worker& target, const std::string& stat, int val) {
+    money -= val * 3;
+    if (stat == "productivity") {
+        target.productivity += val;
+        target.productivity = max(target.productivity, 100);
+    }
+    else if (stat == "energy") {
+        target.energy += val;
+        target.energy = max(target.energy, 100);
+    }
+    else if (stat == "mood") {
+        target.mood += val;
+        target.mood = max(target.mood, 100);
+    }
+    else if (stat == "health") {
+        target.health += val;
+        target.health = max(target.health, 100);
+    }
 
+}
+
+void SeniorNetwork::applyLeadershipBoost(std::vector<JuniorNetwork*>& juniors) {
+    money -= 50;
+    for (auto& junior : juniors) {
+        if (junior->obedient)
+            junior->productivity = max(100, junior->productivity + int((junior->productivity) * 0.2));
+        junior->energy = max(100, junior->energy + int((junior->energy) * 0.2));
+        junior->mood = max(100, junior->mood + int((junior->mood) * 0.2));
+    }
+}
+//target --> curr senior worker
+void SeniorNetwork::medassistance(JuniorNetwork* junior, Worker& target) {
+    target.health -= 10;
+    junior->health = 100;
+}
+
+void SeniorNetwork::gamble() {
+    //need to implement//getting some errors
+}
+void SeniorNetwork::applyGamble(std::vector<Worker*>& workers) {
+    isgambler = true;
+    money -= 50;
+    gamble();
+}
+void SeniorNetwork::callgamble() {
+    //need to implement
 }
--- senior_worker.h
+++ senior_worker.h
@@ -11,12 +11,13 @@ public:
 
     // power -up can be purchased
     bool isgambler = false;
-    void boostJuniorProductivity(std::vector<JuniorSoftware*>& juniors);
-    void applyGamble(std::vector<Worker*>& workers);
-    void applyEnergyBoost(Worker& target);
+    void juniorStatsBoostByPercentage(std::vector<JuniorSoftware*>& juniors,int percentage,std::string &stat);
+    void juniorstatsboostbyval(std::vector<JuniorSoftware*>& juniors, const std::string& stat, int val);
+    void applySelfStatBoost(Worker& target,const std::string & stat,int val);
     void applyLeadershipBoost(std::vector<JuniorSoftware*>& juniors);
-    void applySelectiveStatBoost(std::vector<JuniorSoftware*>& juniors, const std::string& stat);
+    void medassistance(JuniorSoftware* junior, Worker& target);
     void gamble();
+    void applyGamble(std::vector<Worker*>& workers);
     void callgamble();
 };
 
@@ -25,15 +26,16 @@ public:
     SeniorHardware(int index, short int x = 0, short int y = 0, short int gridnumber = 0);
     SeniorHardware(int index, bool gender, char* name, short int x = 0, short int y = 0);
 
-    // power-up
-    void boostHardwareSkills(std::vector<JuniorHardware*>& juniors);
-    void applyGamble(std::vector<Worker*>& workers);
-    void applyEnergyBoost(Worker& target);
+    // power -up can be purchased
+    bool isgambler = false;
+    void juniorStatsBoostByPercentage(std::vector<JuniorHardware*>& juniors, int percentage, std::string& stat);
+    void juniorstatsboostbyval(std::vector<JuniorHardware*>& juniors, const std::string& stat, int val);
+    void applySelfStatBoost(Worker& target, const std::string& stat, int val);
     void applyLeadershipBoost(std::vector<JuniorHardware*>& juniors);
-    void applySelectiveStatBoost(std::vector<JuniorHardware*>& juniors, const std::string& stat);
-
-private:
+    void medassistance(JuniorHardware* junior, Worker& target);
     void gamble();
+    void applyGamble(std::vector<Worker*>& workers);
+    void callgamble();
 };
 
 class SeniorNetwork : public Worker {
@@ -41,17 +43,17 @@ public:
     SeniorNetwork(int index, short int x = 0, short int y = 0, short int gridnumber = 0);
     SeniorNetwork(int index, bool gender, char* name, short int x = 0, short int y = 0);
 
-    //power_up
-    void boostNetworkSkills(std::vector<JuniorNetwork*>& juniors);
-    void applyGamble(std::vector<Worker*>& workers);
-    void applyEnergyBoost(Worker& target);
+    // power -up can be purchased
+    bool isgambler = false;
+    void juniorStatsBoostByPercentage(std::vector<JuniorNetwork*>& juniors, int percentage, std::string& stat);
+    void juniorstatsboostbyval(std::vector<JuniorNetwork*>& juniors, const std::string& stat, int val);
+    void applySelfStatBoost(Worker& target, const std::string& stat, int val);
     void applyLeadershipBoost(std::vector<JuniorNetwork*>& juniors);
-    void applySelectiveStatBoost(std::vector<JuniorNetwork*>& juniors, const std::string& stat);
-
-private:
+    void medassistance(JuniorNetwork* junior, Worker& target);
     void gamble();
+    void applyGamble(std::vector<Worker*>& workers);
+    void callgamble();
 };
-
 
 
 
